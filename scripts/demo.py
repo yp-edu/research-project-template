b776eb68cf2d0e28cfd4c43cd73d3f55bae4cfc2
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2025-04-10 10:57:20 +0200

    Hydra outputs

--- scripts/demo.py
+++ scripts/demo.py
@@ -13,9 +13,9 @@ import hydra
 from omegaconf import DictConfig
 
 
-@hydra.main(config_path="../configs", config_name="demo.yaml")
+@hydra.main(config_path="../configs", config_name="demo.yaml", version_base=None)
 def main(cfg: DictConfig):
-    logger.info(f"This is a demo script with an important parameter: {cfg.important_config_parameter}")
+    logger.info(f"This is a demo script with an important parameter: {cfg.important_parameter}")
 
 
 if __name__ == "__main__":
